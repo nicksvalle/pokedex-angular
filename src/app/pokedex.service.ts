f4b9b292b7b4136989fd83fef1e0d878490b68e4
nicolasdeoliveiravalle@gmail.com <nicolasdeoliveiravalle@gmail.com>
2023-05-04 12:04:37 -0300

    falta muito

--- src/app/pokedex.service.ts
+++ src/app/pokedex.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pokedex } from './pokedex';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokedexService {
 
-  constructor() { }
+
+  constructor( private http: HttpClient ) {
+
+   }
+
+   getPokedex() : Observable<Pokedex> {
+    return this.http.get<Pokedex>("https://pokeapi.co/api/v2/pokemon-form/25")
+   }
 }
--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokedex } from '../pokedex';
+import { PokedexService } from '../pokedex.service';
 
 @Component({
   selector: 'app-pokedex',
@@ -9,8 +10,24 @@ import { Pokedex } from '../pokedex';
 export class PokedexComponent implements OnInit {
   pokedex : Pokedex = {} as Pokedex;
 
+  constructor(private service : PokedexService){}
+
   ngOnInit(): void {
     this.pokedex.front_default = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
   }
 
+  loadPokedex(){
+    this.service.getPokedex().subscribe(
+      {
+        next : data => this.pokedex = data
+      }
+      
+    );
+  }
+  
+  getBreed(): string[] {
+    const parts = this.pokedex.front_default.split('/');
+    const pokemonId = parts[parts.length - 1].split('.')[0];
+    return [pokemonId];
+  }
 }
